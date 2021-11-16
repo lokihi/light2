bb490574f126eb0c12542d07cad05cdcb2b23403
lokihi <90849723+lokihi@users.noreply.github.com>
2021-11-16 20:20:39 +0300

    Add files via upload

--- light/scripts/lightMeasure.py
+++ light/scripts/lightMeasure.py
@@ -1,1 +1,43 @@
+#Подключение библиотек
+import time
+import picamera
 
+
+#Объявление переменных
+experiment_number = 0
+lamps = ["mercury", "tungsten", "tungsten", "tungsten", "tungsten", "tungsten"]
+paper = ["white", "white", "red", "yellow", "green", "blue"]
+
+
+#Функция подготовки камеры
+def prepare_camera():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        time.sleep(10)
+
+
+#Сделать снимок
+def photo(experiment_name):
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        camera.capture(experiment_name + ".png")
+
+#Main part
+print("-----To start experiment type y-----")
+a = input()
+
+if a == "y":
+    print("Preparing the camera...")
+    prepare_camera()
+    print("Camera is ready")
+
+for i in range(6):
+    print("-----Type y to take a photo-----")
+    a = input()
+    if a == 'y':
+        print("Taking a photo for  + paper[i] +  paper +  and  + lamps[i] +  lamp")
+        photo(paper[i] + "-" + lamps[i])
+    else:
+        break
+print("-----End of experiment-----")
